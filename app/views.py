1f200fbd319b43bd6af02f5e6f67a06f468a21b2
Mika Koivusaari <mika.koivusaari@gmail.com>
2016-07-27 22:47:22 +0300

    Templates working.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    temperature = {'outside': 20.0}
+    electricity = {'normal': 1.5}
+
+    return render_template('index.html',
+                           temperature=temperature,
+                           electricity=electricity)
 
